d9f596576acc3ab65d98727cb4a4b0721a2be981
sy-md <73502678+sy-md@users.noreply.github.com>
2022-10-28 19:22:08 +0900

    update to security
    admin passwd given

--- main.go
+++ main.go
@@ -8,9 +8,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Connection URI -- ask the user to log in instead
-const uri = "mongodb+srv://mdorsett:12345@cluster0.erkptpf.mongodb.net/?retryWrites=true&w=majority"
-
 func main() {
 	// Create a new client and connect to the server
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
